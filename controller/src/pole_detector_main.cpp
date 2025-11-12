1bcf6b353d98b0ec3dfcd79ab9158a1967491375
vjimenez2021 <v.jimenez.2021@alumnos.urjc.es>
2025-11-12 17:02:54 +0100

    Changed package name to pole_detection

--- controller/src/pole_detector_main.cpp
+++
@@ -1,8 +0,0 @@
-#include "controller/pole_detector.hpp"
-
-int main(int argc, char **argv) {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<PoleDetector>());
-    rclcpp::shutdown();
-    return 0;
-}
